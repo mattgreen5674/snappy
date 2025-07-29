98d55a089662c99e17b3856aa22afe4a05572a31
Matthew Green <matthew.g@picniq.co.uk>
2025-07-29 07:53:41 +0100

    Remove api shop controller route from old snappy project.

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,15 @@
 <?php
 
-use App\Http\Controllers\ShopController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')
     ->group(function () {
 
-        Route::controller(ShopController::class)
-            ->prefix('shops')
-            ->group(function () {
-                Route::post('', 'create')->name('shops.create');
-                Route::post('near-to-postcode', 'nearToPostCode')->name('shops.near_to_postcode');
-                Route::post('will-deliver-to-postcode', 'willDeliverToPostCode')->name('shops.will_deliver_to_postcode');
-            });
-
         Route::prefix('user')->group(function () {
             Route::get('', function (Request $request) {
                 return $request->user();
             });
         });
+
     });
