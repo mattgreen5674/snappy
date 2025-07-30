b705e8cd52e66f30e98f4e33f99b06c2c0686586
Matthew Green <matthew.g@picniq.co.uk>
2025-07-30 08:41:34 +0100

    Add sports monk import job schedule.

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('app:countries-import')
+    ->dailyAt('01:00')
+    ->environments('production');
+
+Schedule::command('app:players-import')
+    ->everyOddHour() // May need to consider if this is too much or too little!
+    ->unlessBetween('23:55', '0:05')
+    ->environments('production');
