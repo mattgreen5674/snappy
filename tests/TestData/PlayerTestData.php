bd31f1e0b4c6cdfc861d57172d99106c04af1d6a
Matthew Green <matthew.g@picniq.co.uk>
2025-08-06 10:09:39 +0100

    Add player test data class to save us having to constantly rewrite the require data items into the DB.

---
+++ tests/TestData/PlayerTestData.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\TestData;
+
+use App\Models\Countries\Country;
+use App\Models\Players\Player;
+use App\Models\Players\Position;
+use Illuminate\Support\Arr;
+
+class PlayerTestData
+{
+    public static function build(int $playersToBuild = 1): array
+    {
+        $dbCountry        = Country::factory()->create(['external_country_id' => 1000, 'name' => 'TestCountry']);
+        $dbPosition       = Position::factory()->create(['external_position_id' => 1000, 'name' => 'TestPosition']);
+        $dbParentPosition = Position::factory()->create(['external_position_id' => 1001, 'name' => 'TestParentPosition']);
+        $dbPlayers        = collect();
+        for ($i = 1; $i <= $playersToBuild; $i++)
+        {
+            $dbPlayer         = Player::factory()->create([
+                'first_name'         => 'Test',
+                'last_name'          => 'Player',
+                'nationality_id'     => $dbCountry->external_country_id,
+                'position_id'        => $dbPosition->external_position_id,
+                'parent_position_id' => $dbParentPosition->external_position_id,
+            ]);
+
+            $dbPlayers->push($dbPlayer);
+        }    
+        
+        $data = [
+            'country'        => $dbCountry,
+            'position'       => $dbPosition,
+            'parentPosition' => $dbParentPosition,
+        ];
+
+        $data = ($playersToBuild == 1)
+            ? array_merge($data, ['player'  => $dbPlayers->first()])
+            : array_merge($data, ['players' => $dbPlayers]);
+        
+        return $data;
+    }
+}
